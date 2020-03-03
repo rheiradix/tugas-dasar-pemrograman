e653e979094727bb83ab32f1ebb7b1879ea54c14
rheiradix <61744319+rheiradix@users.noreply.github.com>
2020-03-03 22:04:21 +0700

    Add files via upload

---
+++ tugas 4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int umur;
+	cout<<"umur kamu pensiun polri? ";
+	cin>>umur;
+	
+	if(umur>=58){cout<<"ya,kamu pensiun dari polri!:)"<<endl;
+
+}
+	   
+	   else{
+	   	cout<<"tidak,kamu belum pensiun dari polri!"<<endl;
+	   }
+	
+
+	
+}
+
+
